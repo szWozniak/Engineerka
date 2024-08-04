61ef40df1521ee9388944c1a23427ce4cc7258c9
szWozniak <szymon.wozniak@placewise.com>
2024-08-04 13:57:54 +0200

    DRONHUB-72: QoL zmiany do sidebara + "Usuniecie strzalki z "Filtry" na lewym sidebarze"

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -26,43 +26,38 @@ const Sidebar: React.FC = () => {
           <CloseIcon />
         </button>
       </div>
-      <div className="container">
-        <h3>Menu</h3>
-        <MenuDropdown
-          label={"Filtry"}
-          opened={openedMenu === 1}
-          setOpened={(opened) => {
-            opened ? setOpenedMenu(1) : setOpenedMenu(null)
-          }}
-        />
-        <MenuDropdown
-          label={"Moje Drony"}
-          opened={openedMenu === 2}
-          setOpened={(opened) => {
-            opened ? setOpenedMenu(2) : setOpenedMenu(null)
-          }}
-        />
-        {openedMenu === 2 && <div className="droneEntries">
-          {drones?.map((drone: Drone, index) => (
-            <div key={index} className="droneEntry" onClick={() => {
-              setSelectedDroneRegistration(drone.registrationNumber)
-            }}>
-              <span>{drone.identification}. </span>
-              <span>{drone.registrationNumber} </span>
-              <span className="extraLabel">{drone.type}</span>
-            </div>
-          ))}
-        </div>}
-      </div>
-      <div className="container">
-        {selectedDrone && <div>
-          <div>Selected drone: {selectedDrone.registrationNumber}</div>
-          <div>Latitude: <b>{selectedDrone.currentPosition.latitude.toFixed(4)}</b></div>
-          <div>Longtitude: <b>{selectedDrone.currentPosition.longitude.toFixed(4)}</b></div>
-          <div>Direction: <b>{selectedDrone.heading}</b></div>
-          <div>Altitude: <b>{selectedDrone.currentPosition.altitude}</b></div>
-          {renderViewChangeButtons()}
-        </div>}
+      <div className="scrollable">
+        <div className="container">
+          <h3>DronHub</h3>
+          <MenuDropdown
+            label={"Moje Drony"}
+            opened={openedMenu === 1}
+            setOpened={(opened) => {
+              opened ? setOpenedMenu(1) : setOpenedMenu(null)
+            }}
+          />
+          {openedMenu === 1 && <div className="droneEntries">
+            {drones?.map((drone: Drone, index) => (
+              <div key={index} className="droneEntry" onClick={() => {
+                setSelectedDroneRegistration(drone.registrationNumber)
+              }}>
+                <span>{drone.identification}. </span>
+                <span>{drone.registrationNumber} </span>
+                <span className="extraLabel">{drone.type}</span>
+              </div>
+            ))}
+          </div>}
+        </div>
+        <div className="container">
+          {selectedDrone && <div>
+            <h3>{selectedDrone.registrationNumber}</h3>
+            <div>Latitude: <b>{selectedDrone.currentPosition.latitude.toFixed(4)}</b></div>
+            <div>Longtitude: <b>{selectedDrone.currentPosition.longitude.toFixed(4)}</b></div>
+            <div>Direction: <b>{selectedDrone.heading}</b></div>
+            <div>Altitude: <b>{selectedDrone.currentPosition.altitude}</b></div>
+            {renderViewChangeButtons()}
+          </div>}
+        </div>
       </div>
       <div className="container">
         DronHub.pl &copy; 2024
