f6ef3d39d6807dbfd816faf507deb7fe74a48b2b
ArkadiuszMin <arernonokakaka2@gmail.com>
2024-08-22 20:30:03 +0200

    WORKING

--- backend/src/main/java/com/example/backend/simulatorIntegration/model/DroneFromSimulator.java
+++ backend/src/main/java/com/example/backend/simulatorIntegration/model/DroneFromSimulator.java
@@ -147,4 +147,5 @@ public class DroneFromSimulator {
         this.sensorLabel = sensorLabel;
     }
 
+    public DroneFromSimulator(){}
 }
--- backend/src/test/java/com/example/backend/simulatorIntegration/DummyFilePathProvider.java
+++ backend/src/test/java/com/example/backend/simulatorIntegration/DummyFilePathProvider.java
@@ -5,7 +5,7 @@ import java.net.URL;
 public class DummyFilePathProvider {
     public String getPath(){
         ClassLoader loader = getClass().getClassLoader();
-        URL resource = loader.getResource("droneFromSimulator.csv");
+        URL resource = loader.getResource("dane.csv");
         return resource.getPath();
     }
 }
