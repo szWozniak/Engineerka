bebe7d96728dc0b36729fc189ef1913e58b05dfc
ArkadiuszMin <arernonokakaka2@gmail.com>
2024-08-22 19:59:25 +0200

    sth no yes

--- backend/src/test/java/com/example/backend/simulatorIntegration/DummyFilePathProvider.java
+++ backend/src/test/java/com/example/backend/simulatorIntegration/DummyFilePathProvider.java
@@ -1,12 +1,11 @@
 package com.example.backend.simulatorIntegration;
 
 import java.net.URL;
-import java.nio.file.Paths;
 
 public class DummyFilePathProvider {
     public String getPath(){
         ClassLoader loader = getClass().getClassLoader();
-        URL resource = loader.getResource("dane.csv");
-        return Paths.get(resource.getPath()).toString();
+        URL resource = loader.getResource("droneFromSimulator.csv");
+        return resource.getPath();
     }
 }
