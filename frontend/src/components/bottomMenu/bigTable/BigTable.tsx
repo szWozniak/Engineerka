0f7f2bd436a6d723e4d9d44c80a90813e093b721
szWozniak <szymon.wozniak@placewise.com>
2024-09-01 13:44:34 +0200

    DRONHUB-62: Big Table - dodanie danych geograficznych, zmiana stylowania

--- frontend/src/components/bottomMenu/bigTable/BigTable.tsx
+++ frontend/src/components/bottomMenu/bigTable/BigTable.tsx
@@ -1,27 +1,49 @@
 import { useEffect } from "react";
-import { DroneBase } from "../../../drones/types";
+import { Drone, DroneBase } from "../../../drones/types";
 
 interface props {
-  drones: DroneBase[] | undefined
+  drones: Drone[] | undefined
+  allDrones: DroneBase[] | undefined
 }
 
-const BigTable: React.FC<props> = ({drones}) => {  
+const BigTable: React.FC<props> = ({drones, allDrones}) => {  
+  
   return (
     <div className="content" style={{"height": "230px"}}>
       Lista Dronów
       <table className="droneTable">
         <thead>
           <tr>
-            <th>Nr. rejestracyjny</th>
-            <th>Typ drona</th>
+            <th rowSpan={2}>Nr. rejestracyjny</th>
+            <th colSpan={3}>Dane geograficzne</th>
+            <th rowSpan={2}>Operator</th>
+            <th rowSpan={2}>Model</th>
+            <th rowSpan={2}>Typ drona</th>
+          </tr>
+          <tr>
+            <th>Szerokość</th>
+            <th>Długość</th>
+            <th>Wysokość</th>
           </tr>
         </thead>
-        {drones?.map((drone: DroneBase, index) => (
-        <tr key={index} className="droneEntry" >
-          <td>{drone.registrationNumber} </td>
-          <td className="extraLabel">{drone.type}</td>
-        </tr>
-        ))}
+        {allDrones?.map((drone: DroneBase, index) => {
+          const flyingDrone = drones?.find(d => d.registrationNumber === drone.registrationNumber)
+
+          return (
+            <tr key={index} className="droneEntry" >
+              <td>{drone.registrationNumber} </td>
+              {flyingDrone ? <>
+                <td>{flyingDrone?.currentPosition?.latitude?.toFixed(4)}</td>
+                <td>{flyingDrone?.currentPosition?.longitude?.toFixed(4)}</td>
+                <td>{flyingDrone?.currentPosition?.altitude}</td>
+              </> : <td colSpan={3}>b/d
+                </td>}
+              <td>{drone.operator} </td>
+              <td>{drone.model} </td>
+              <td className="extraLabel">{drone.type}</td>
+            </tr>
+            )
+        })}
       </table>
     </div>
   )
