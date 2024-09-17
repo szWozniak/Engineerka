2d99a1e012c289c294ae6c2a568708d5e052701c
szWozniak <szymon.wozniak@placewise.com>
2024-09-17 17:59:52 +0200

    BottomMenu resizer

--- frontend/src/components/bottomMenu/BottomMenu.tsx
+++ frontend/src/components/bottomMenu/BottomMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { ArrowUpIcon } from '../icons/ArrowUpIcon';
 import { ArrowDownIcon } from '../icons/ArrowDownIcon';
 import { AppContext } from '../../context/AppContext';
@@ -9,13 +9,60 @@ const BottomMenu = () => {
   const [isOpened, setIsOpened] = useState(false)
   const { areFiltersOpened } = useContext(AppContext)
 
+  const [startY, setStartY] = useState(0)
+  const [startHeight, setStartHeight] = useState(0)
+  const [size, setSize] = useState(430)
+
+  const isResizing = useRef(false)
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    isResizing.current = true
+    setStartY(e.clientY)
+    setStartHeight(size)
+    document.body.style.cursor = 'ns-resize'
+  }
+
+  const handleMouseUp = () => {
+    if(isResizing.current) {
+      isResizing.current = false
+      document.body.style.cursor = ''
+    }
+  }
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if(isResizing.current) {
+      setSize(window.innerHeight - e.clientY)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [])
+
   return (
-    <div className={`bottomMenu ${isOpened && 'opened'}`}>
-      <div className="shadowArea" onClick={() => setIsOpened(prev => !prev)}>
+    <div 
+      className={`bottomMenu ${isOpened && 'opened'}`}
+      style={{"transform": `translateY(${isOpened ? 0 : (size + 20)}px)`}}
+    >
+      <div 
+        className="shadowArea" 
+        style={{"height": size }} 
+        onClick={() => setIsOpened(prev => !prev)}
+      >
         {isOpened ? <ArrowDownIcon /> : <ArrowUpIcon />}
       </div>
       {areFiltersOpened && <FilterSection isOpen={areFiltersOpened}/>}
-      <BigTable />
+      <div className="content" style={{"height": size}}>
+        <div className="resizer" onMouseDown={handleMouseDown}></div>
+        <span>Lista Dronów</span>
+        <BigTable />
+      </div>
     </div>
   );
 };
