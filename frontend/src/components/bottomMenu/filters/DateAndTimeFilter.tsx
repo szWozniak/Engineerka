38d4537fe9355651d5f15a8fdf562482008b0dc8
ArkadiuszMin <arernonokakaka2@gmail.com>
2024-10-20 20:27:11 +0200

    lol

---
+++ frontend/src/components/bottomMenu/filters/DateAndTimeFilter.tsx
@@ -0,0 +1,33 @@
+import React, { useMemo } from 'react';
+import { IoClose } from 'react-icons/io5';
+
+interface Props{
+    label: string,
+    value: string,
+    onChange: (value: string) => void
+    onReset: () => void
+}
+
+const DateAndTimeFilter: React.FC<Props> = ({
+    label,
+    value,
+    onChange,
+    onReset
+}) => {
+    const displayValue = useMemo(() => value, [value])
+    
+    return (
+        <div className='filterContent'>
+            {label}
+            <div className='actionContainer'>
+                <input type="datetime-local" onChange={(e) => onChange(e.target.value)} value={displayValue} className="concreteFilter"/>
+            </div>
+            <button className="clear-icon" onClick={onReset}>
+                <IoClose />
+            </button>
+        </div>
+    );
+};
+
+
+export default DateAndTimeFilter;
