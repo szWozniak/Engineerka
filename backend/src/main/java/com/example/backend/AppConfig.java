40cad1d26ddb716d24af4eda143ac7c480fb5c55
Pejdzor <114942872+Pejdzor@users.noreply.github.com>
2024-11-09 15:53:01 +0100

    Napraw porty w corsach (#81)
    Signed-off-by: Pejdzor <mikolaj.pajor@gmail.com>

--- backend/src/main/java/com/example/backend/AppConfig.java
+++ backend/src/main/java/com/example/backend/AppConfig.java
@@ -18,7 +18,7 @@ public class AppConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://dronhub.pl","http://localhost","http://frontend");
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080","http://dronhub.pl","http://dronhub.pl:8080","http://localhost");
             }
         };
     }
