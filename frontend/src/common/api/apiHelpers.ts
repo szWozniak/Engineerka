cd2f692f2fc20351c3a1fb84be5baed47439e740
Pejdzor <114942872+Pejdzor@users.noreply.github.com>
2024-11-10 12:06:22 +0100

    Update apiHelpers.ts (#83)

--- frontend/src/common/api/apiHelpers.ts
+++ frontend/src/common/api/apiHelpers.ts
@@ -1,4 +1,4 @@
-export const defaultURL = "http://dronhub.pl:8080"
+export const defaultURL = "http://localhost:8080"
 
 export const checkForErrors = (res: Response) => {
     if (res.status !== 200) {
